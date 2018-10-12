9a4202c2bbbf8bd30b56496f9706ef72aee0395d
JarlTuxen <jart@kea.dk>
2018-10-12 08:58:34 +0200

    first arraysmetode

---
+++ kode/ArrayMetoder.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArrayMetoder
+{
+   public static void main(String[] args)
+   {
+      int[] tal = new int[3];
+      tal[0] = 3;
+      tal[1] = 8;
+      tal[2] = 2;
+      
+      System.out.println(Arrays.toString(tal));
+   
+   }
+
+}
